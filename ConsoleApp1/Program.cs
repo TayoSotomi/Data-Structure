2dde4ffc1bd7e4cfab786dcba52460e2e097f09a
Tee <sotomi.tayo@gmail.com>
2023-02-02 15:27:46 -0600

    First submit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Linq;
+using System.Reflection.PortableExecutable;
+
+Console.WriteLine("Please enter a word");
+string input = Console.ReadLine();
+Console.WriteLine(ReverseInput(input));
+
+
+
+
+
+
+
+
+
+
+
+
+//method 
+  static string ReverseInput(string input)
+{
+    Stack<char> stack = new Stack<char>();
+      foreach(char s in input)
+    {
+        stack.Push(s); 
+    }
+    string result = "";
+    foreach(char c in stack)
+    {
+        result += c;
+    }
+    return result;
+}
+    
+
+
+
+
+    //foreach(char c in string.Join(",", stack.ToArray()))
+    //{
+    //    stack .Pop();
+    //}
+
+
